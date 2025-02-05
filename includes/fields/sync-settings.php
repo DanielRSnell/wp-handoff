4c4c7efe68799137a1efe0f6ade37aa4594dff3c
Artisan <daniel@domartisan.com>
2025-02-05 03:26:21 -0500

    Automated block registration with Twig

--- includes/fields/sync-settings.php
+++ includes/fields/sync-settings.php
@@ -4,28 +4,18 @@ if (! function_exists('acf_add_local_field_group')) {
     return;
 }
 
-/**
- * ACF JSON Save/Load Configuration
- */
-
-// Set up ACF JSON save points
-function my_acf_json_save_point($path)
-{
+function my_acf_json_save_point($path) {
     return get_stylesheet_directory() . '/src/json-store';
 }
 add_filter('acf/settings/save_json', 'my_acf_json_save_point');
 
-// Set up ACF JSON load points
-function my_acf_json_load_point($paths)
-{
+function my_acf_json_load_point($paths) {
     $paths[] = get_stylesheet_directory() . '/src/json-store';
     return $paths;
 }
 add_filter('acf/settings/load_json', 'my_acf_json_load_point');
 
-// Customize ACF JSON save paths based on field type
-function custom_acf_json_save_paths($paths, $post)
-{
+function custom_acf_json_save_paths($paths, $post) {
     $directory = get_stylesheet_directory() . '/src/json-store';
 
     switch ($post['type']) {
@@ -55,19 +45,13 @@ function custom_acf_json_save_paths($paths, $post)
 }
 add_filter('acf/json/save_paths', 'custom_acf_json_save_paths', 10, 2);
 
-/**
- * Block Registration
- */
-
-function register_acf_blocks()
-{
+function register_acf_blocks() {
     $blocks_path = get_stylesheet_directory() . '/src/blocks';
-    if (! is_dir($blocks_path)) {
+    if (!is_dir($blocks_path)) {
         return;
     }
 
-    // Get all block categories
-    $categories = [];
+    $block_categories = [];
     $block_dirs = glob($blocks_path . '/*', GLOB_ONLYDIR);
 
     foreach ($block_dirs as $block_dir) {
@@ -75,42 +59,31 @@ function register_acf_blocks()
         if (file_exists($block_json_file)) {
             $block_data = json_decode(file_get_contents($block_json_file), true);
             if (isset($block_data['category'])) {
-                $categories[$block_data['category']] = true;
+                $block_categories[$block_data['category']] = true;
             }
         }
     }
 
-    // Register block categories
-    add_filter('block_categories_all', function ($cats) use ($categories) {
-        foreach (array_keys($categories) as $category) {
-            $cats[] = [
-                'slug'  => sanitize_title($category),
+    add_filter('block_categories_all', function ($categories) use ($block_categories) {
+        foreach (array_keys($block_categories) as $category) {
+            $categories[] = [
+                'slug' => sanitize_title($category),
                 'title' => ucwords(str_replace('-', ' ', $category)),
-                'icon'  => null,
+                'icon' => null,
             ];
         }
-        return $cats;
+        return $categories;
     });
 
-    // Register blocks
     foreach ($block_dirs as $block_dir) {
-        $block_json_file = $block_dir . '/block.json';
-        if (file_exists($block_json_file)) {
-            // TODO: Fixed The Following
-            // This should register the path to each block, not the JSON file specifically
-            register_block_type($block_json_file);
-        }
+        register_block_type($block_dir);
     }
 }
 add_action('init', 'register_acf_blocks');
 
-/**
- * Register Post Types
- */
-function register_custom_post_types()
-{
+function register_custom_post_types() {
     $post_types_dir = get_stylesheet_directory() . '/src/register/post-type';
-    if (! is_dir($post_types_dir)) {
+    if (!is_dir($post_types_dir)) {
         return;
     }
 
@@ -121,13 +94,9 @@ function register_custom_post_types()
 }
 add_action('init', 'register_custom_post_types');
 
-/**
- * Register Taxonomies
- */
-function register_custom_taxonomies()
-{
+function register_custom_taxonomies() {
     $taxonomies_dir = get_stylesheet_directory() . '/src/register/taxonomy';
-    if (! is_dir($taxonomies_dir)) {
+    if (!is_dir($taxonomies_dir)) {
         return;
     }
 
@@ -138,13 +107,9 @@ function register_custom_taxonomies()
 }
 add_action('init', 'register_custom_taxonomies');
 
-/**
- * Register Options Pages
- */
-function register_options_pages()
-{
+function register_options_pages() {
     $options_dir = get_stylesheet_directory() . '/src/register/options';
-    if (! is_dir($options_dir)) {
+    if (!is_dir($options_dir)) {
         return;
     }
 
--- src/blocks/accordion/block-controller.php
+++ src/blocks/accordion/block-controller.php
@@ -1,6 +1,7 @@
 <?php
-$context = Timber::context();
-$context['block'] = $block;
+
+$context           = Timber::context();
+$context['block']  = $block;
 $context['fields'] = get_fields();
 
 Timber::render('@block/accordion/block.twig', $context);
